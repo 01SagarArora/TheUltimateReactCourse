9eb6c109f29399a30a67f273aa282cc3cb5bce84
SagarArora <01sagar.arora@gmail.com>
2023-11-30 10:49:10 +0530

    test

--- steps/src/App.js
+++ steps/src/App.js
@@ -11,7 +11,7 @@ export default function App() {
 const questions = [
   {
     id: 3457,
-    question: 'What language is React based on?',
+    question: 'What language the is React based on?',
     answer: 'JavaScript',
   },
   {

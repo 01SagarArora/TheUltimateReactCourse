b10c0bde2a3132ed57ba7271b4305efe4eefa1bb
YATRAONLINE\sagar.arora <01sagar.arora@gmail.com>
2023-11-30 18:34:38 +0530

    Test

--- FarAway/src/App.js
+++ FarAway/src/App.js
@@ -59,7 +59,7 @@ function Form({ onAddItems }) {
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip ?</h3>
+      <h3>Hi What do you need for your 😍 trip ?</h3>
       <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>

c9ee4823ae5cdb2aa92353743b2478b10f3c6c06
SagarArora <sagar.arora@surakshitnivesh.com>
2023-12-13 11:33:35 +0530

    Lecture 119

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import StarRating from "./StarRating"
+import StarRating from "./StarRating";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      color="red"
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
   </React.StrictMode>
 );

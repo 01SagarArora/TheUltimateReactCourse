8d419a733eb2ef6475132271b653cb79d1b99144
Sagar Arora <01sagar.arora@gmail.com>
2024-01-26 12:07:38 +0530

    Lecture 214 - Implementing the Countries List

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { useEffect,useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import HomePage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import CityList from './components/CityList';
-import { useEffect,useState } from 'react';
+import CountriesList from './components/CountryList';
 
 const BASE_URL = `http://localhost:9000`
 
@@ -41,7 +42,7 @@ function App() {
         <Route path="app" element={<AppLayout />} >
         <Route index element={<CityList cities={cities} isLoading={isLoading} /> } />
          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} /> } />
-         <Route path="countries" element={<p>Countries</p>} />
+         <Route path="countries" element={<CountriesList cities={cities} isLoading={isLoading} />} />
          <Route path="form" element={<p>Form</p>} />
         </Route> 
         <Route path="*" element={<PageNotFound />} />

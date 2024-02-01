2027c649a63e0fe8cc133623567fecd17c3daa35
Sagar Arora <01sagar.arora@gmail.com>
2024-02-01 18:38:18 +0530

    217 - Reading and Setting a Query String

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -12,10 +12,10 @@ const formatDate = (date) =>
 
 function CityItem({ city }) {
 
-    const { cityName, emoji, date, id } = city;
+    const { cityName, emoji, date, id, position } = city;
 
   return (
-    <Link className={styles.cityItem} to={`${id}`} >        
+    <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`} >        
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>{date}</time>

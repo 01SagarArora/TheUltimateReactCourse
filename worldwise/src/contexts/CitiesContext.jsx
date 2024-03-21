aecc479e286892c3400b120562e05344527f6080
Sagar Arora <sagararora@Sagars-MacBook-Pro.local>
2024-03-21 22:08:06 +0530

    236 - Deleting a City

--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -42,20 +42,34 @@ function CitiesProvider({ children }) {
   async function createCity(newCity) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities`,
-      {
-        method:'POST',
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
         body: JSON.stringify(newCity),
         headers: {
           "Content-Type": "application/json",
-        }
-      })
+        },
+      });
       const data = await res.json();
-      setCities((cities) => [...cities, data])
+      setCities((cities) => [...cities, data]);
       console.log(data);
       setCurrentCity(data);
     } catch {
-      alert("There was an error loading data...");
+      alert("There was an error creating city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error deleting city...");
     } finally {
       setIsLoading(false);
     }
@@ -68,7 +82,8 @@ function CitiesProvider({ children }) {
         isLoading,
         currentCity,
         getCity,
-        createCity
+        createCity,
+        deleteCity,
       }}
     >
       {children}
